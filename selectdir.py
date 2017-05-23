1cacf24d53403d33e104db87efdb6794b2d211ce
avi-nitp <avinashsinghrajput97@gmail.com>
2017-05-23 15:02:47 +0530

    Delete selectdir.py

--- selectdir.py
+++
@@ -1,8 +0,0 @@
-import tkinter, tkinter.filedialog
-import os
-root = tkinter.Tk()
-dirname = tkinter.filedialog.askdirectory(parent=root,initialdir="/home",title='Please select a directory')
-if len(dirname ) > 0:
-    print(dirname)
-os.system('python3 maincheck.py')
-root.mainloop()
